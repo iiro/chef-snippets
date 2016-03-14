f4553b09ca3217102a07fa16be0c3bb70d2d287c
Iiro Niinikoski <iiro.niinikoski@nordcloud.fi>
2016-03-14 22:58:20 +0200

    Created an own policies dir and separated policies for Apache and Nginx examples.

--- test-kitchen/policies/web_nginx.rb
+++ test-kitchen/policies/web_nginx.rb
@@ -4,14 +4,18 @@
 # https://github.com/opscode/chef-dk/blob/master/POLICYFILE_README.md
 
 # A name that describes what the system you're building with Chef does.
-name "example_cookbook"
+name 'web_nginx'
 
 # Where to find external cookbooks:
 default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list "example_cookbook::default"
+# run_list "example_cookbook::default"
+run_list(
+  'apt::default',
+  'nginx::default'
+)
 
 # Specify a custom source for a single cookbook:
-cookbook "example_cookbook", path: "."
+# cookbook "example_cookbook", path: "."
 # cookbook "example_common", path: "../example_common"
